652974a31d9f964b4a0533822811ea2cee866526
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-04-15 23:22:06 +0530

    ReverseLinkedList

---
+++ src/LinkedList/ReverseSingleList.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+import static LinkedList.InsertNodeAtBegining.InsertNode;
+import static LinkedList.InsertNodeAtBegining.PrintList;
+
+public class ReverseSingleList {
+    public static void main(String[] args){
+        ListNode head = null; // head of the LinkedList
+        head = InsertNode(40, head);
+        head = InsertNode(30, head);
+        head = InsertNode(20, head);
+        head = InsertNode(10, head);
+        System.out.println("Original LinkedList: ");
+        PrintList(head);
+        head = reverse(head);
+        System.out.println("Reversed LinkedList: ");
+        PrintList(head);
+
+    }
+    public static ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
+        }
+        return prev;
+    }
+}
