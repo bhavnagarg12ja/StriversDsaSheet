abe4e6c5afdbe8c93c8b8d72538987f7956f2d4e
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-04-24 23:39:09 +0530

    subsequence in recursion

---
+++ src/Recursion/Subsequence.java
@@ -0,0 +1,34 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsequence {
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 2};
+        List<List<Integer>> subsequences = generateSubsequences(nums);
+        // Print all subsequences
+        for (List<Integer> subsequence : subsequences) {
+            System.out.println(subsequence);
+        }
+    }
+
+    public static List<List<Integer>> generateSubsequences(int[] nums) {
+        List<List<Integer>> subsequences = new ArrayList<>();
+        generateSubsequences(nums, 0, new ArrayList<>(), subsequences);
+        return subsequences;
+    }
+
+    private static void generateSubsequences(int[] nums, int index, List<Integer> current, List<List<Integer>> subsequences) {
+        if (index == nums.length) {
+            subsequences.add(new ArrayList<>(current));
+            return;
+        }
+        // Include current element
+        current.add(nums[index]);
+        generateSubsequences(nums, index + 1, current, subsequences);
+        current.remove(current.size() - 1); // Backtrack
+        // Exclude current element
+        generateSubsequences(nums, index + 1, current, subsequences);
+    }
+}
