340ad0d0e43414e1641f3dfee325828665de5858
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-04-30 22:44:27 +0530

    myAtoi

---
+++ src/Recursion/SubSequenceWithSumK.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSequenceWithSumK {
+    public static void main(String[]args){
+
+
+    }
+//    public static List<List<Integer>> generateSubSequence(int[]arr){
+//        List<List<Integer>> subSequences = new ArrayList<>();
+//
+//    }
+//    public static void findingSequence(int[] nums, int index, int sum, List<Integer> current, List<List<Integer>> subSequences){
+//
+//
+//    }
+}
--- src/Strings/MyAtoi.java
+++ src/Strings/MyAtoi.java
@@ -6,12 +6,37 @@ public class MyAtoi {
         int result = myAtoi(s);
         System.out.println(result);
     }
-    public static int myAtoi(String s) {
-        if (s == null || s.length() == 0)
-            return 0;
-        s = s.trim();
-        
 
-        return 0;
+    public static int myAtoi(String s) {
+        s = s.trim();
+        if(s==null || s.length() == 0){
+            return 0;
+        }
+        boolean isNegative = false;
+        if (s.charAt(0) == '-') {
+            isNegative = true; // negative decided
+        }
+        double result = 0;
+        int startIndex = 0;
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+            startIndex++;
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                break;
+            }
+            int digit = s.charAt(i)-'0';
+            result = result*10 + digit;
+        }
+        if(isNegative){
+            result = -result;
+        }
+        if(result<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }
+        if(result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return (int)result;
     }
 }
