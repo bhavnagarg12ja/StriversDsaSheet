17567caa7a4cf84cba1eedfa82ba569cc9f95585
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-05-02 23:08:42 +0530

    Subsequence with sum k

--- src/Recursion/SubSequenceWithSumK.java
+++ src/Recursion/SubSequenceWithSumK.java
@@ -5,15 +5,32 @@ import java.util.List;
 
 public class SubSequenceWithSumK {
     public static void main(String[]args){
-
-
+        int[] nums = {1, 2, 1};
+        List<List<Integer>> subsequences = findingSequence(nums,2);
+        // Print all subsequences
+        for (List<Integer> subsequence : subsequences) {
+            System.out.println(subsequence);
+        }
     }
-//    public static List<List<Integer>> generateSubSequence(int[]arr){
-//        List<List<Integer>> subSequences = new ArrayList<>();
-//
-//    }
-//    public static void findingSequence(int[] nums, int index, int sum, List<Integer> current, List<List<Integer>> subSequences){
-//
-//
-//    }
+    public static List<List<Integer>> findingSequence(int[]nums, int sum){
+        List<List<Integer>> subsequences = new ArrayList<>();
+        findingSequence(nums, 0, 0, sum, new ArrayList<>(), subsequences);
+        return subsequences;
+    }
+
+    public static void findingSequence(int[] nums, int index, int s, int sum, List<Integer> current, List<List<Integer>> subSequences) {
+        if (index == nums.length) {
+            if (s == sum) {
+                subSequences.add(new ArrayList<>(current));
+            }
+            return;
+        }
+        current.add(nums[index]);
+        s += nums[index];
+        findingSequence(nums, index + 1, s, sum, current, subSequences);
+        current.remove(current.size() - 1); // Backtrack
+        s -= nums[index];
+        // Exclude current element
+        findingSequence(nums, index + 1, s, sum, current, subSequences);
+    }
 }
--- src/Recursion/countGoodNumbers.java
+++ src/Recursion/countGoodNumbers.java
@@ -12,7 +12,6 @@ public class countGoodNumbers {
     }
     public static long pow(long x, long n){
         if(n==0) return 1;
-
         long temp = pow(x,n/2);
         if(n%2==0){
             return (temp*temp)%mod;
